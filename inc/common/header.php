01bf192378e7bfdeb2e156261523d63605c1cd2f
D4mer <d4mer@d4mations.com>
2017-04-21 19:19:40 +0200

    added font awesome link. Removed logo img. Temp add css styling. will remove and add to css sheet later

--- inc/common/header.php
+++ inc/common/header.php
@@ -17,7 +17,33 @@ global $header_info;
 <script type="text/javascript" src="inc/scripts/ajax_scripts.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script>src="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"</script>
+<style>
+    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
+    .row.content {height: 1500px}
 
+    /* Set gray background color and 100% height */
+    .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height: auto;}
+    }
+</style>
 <?=$header_info['js_scripts']?>
 <!-- Favicon -->
 <link rel="icon" href="favicon.ico" type="image/x-icon" />
@@ -53,7 +79,7 @@ $(document).ready(function() {
     <div class="container">
         <div class="row">
             <div class="col-md-3 sidenav hidden-xs">
-                <a href="<?=BASE_LINK_URL?>"><img src="inc/images/logo.gif" width="407" height="65" alt="Twando" /></a>
+                <a href="<?=BASE_LINK_URL?>"><h2><strong>TWANDO</strong></h2></a>
                 <!--<h2>Logo</h2>-->
               <ul class="nav nav-pills nav-stacked">
                 <li class="active"><a href="http://twando:9001/">Dashboard</a></li>
