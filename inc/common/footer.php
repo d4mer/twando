a66f2e8138c5f7c7a1d2e7e3dcae977c8d19e4d9
D4mer <d4mer@d4mations.com>
2017-04-21 17:06:21 +0200

    Added footer info and sign up form

--- inc/common/footer.php
+++ inc/common/footer.php
@@ -2,7 +2,12 @@
 <!-- End of centre main -->
 </div>
 
-<div id="footer">
+<div id="footer" class="container-fluid text-center">
+  <p>Based on the work of Jon Hudghton</p>
+  <form class="form-inline">Register for more details
+    <input type="email" class="form-control" size="50" placeholder="Email Address">
+    <button type="button" class="btn btn-danger">Register</button>
+  </form>
 </div>
 
 <!-- End of container -->
