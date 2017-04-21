b0d601fc6752f1fa99b213d9ecdfc5c1fa1ded8c
D4mer <d4mer@d4mations.com>
2017-04-21 18:31:15 +0200

    removed sign up form

--- inc/common/footer.php
+++ inc/common/footer.php
@@ -3,14 +3,10 @@
     </div>
 </div>
 
-<div id="footer" class="container-fluid text-center">
-  <p>Based on the work of Jon Hudghton</p>
-  <form class="form-inline">Register for more details
-    <input type="email" class="form-control" size="50" placeholder="Email Address">
-    <button type="button" class="btn btn-danger">Register</button>
-  </form>
-</div>
 
+<footer id="footer" class="container-fluid">
+  <p>Based on the work of Jon Hudghton</p>
+</footer>
 <!-- End of container -->
 </div>
 
