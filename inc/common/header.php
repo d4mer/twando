369cfc433f6f9a9625197bb16dcb127db042ca37
D4mer <d4mer@d4mations.com>
2017-04-21 15:57:16 +0200

    added bootstrap links and modified divs for bootstrap

--- inc/common/header.php
+++ inc/common/header.php
@@ -8,11 +8,16 @@ global $header_info;
 <base href="<?=BASE_LINK_URL?>" />
 <title>Twando</title>
 <meta name="robots" content="noindex, follow" />
-<!-- Style -->
-<link href="inc/style/style.css" rel="stylesheet" type="text/css" />
+<!-- Style
+<link href="inc/style/style.css" rel="stylesheet" type="text/css" />-->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <!-- Scripts -->
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script type="text/javascript" src="inc/scripts/ajax_scripts.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 <?=$header_info['js_scripts']?>
 <!-- Favicon -->
 <link rel="icon" href="favicon.ico" type="image/x-icon" />
@@ -33,15 +38,26 @@ $(document).ready(function() {
 <body>
 
 <!-- Start of container -->
-<div id="container">
+<div id="container" class="container-fluid">
 
 <!-- Header -->
 
-<div id="header_main">
-  <a href="<?=BASE_LINK_URL?>"><img src="inc/images/logo.gif" width="407" height="65" alt="Twando" /></a>
+<div id="header_main" class="navbar navbar-inverse">
+    <div class="navbar-header">
+ <!-- <a href="<?=BASE_LINK_URL?>"><img src="inc/images/logo.gif" width="407" height="65" alt="Twando" /></a>-->
+    </div>
 </div>
 
 <!-- Centre main -->
-<div id="centre_main">
+<div id="centre_main" class="container-fluid">
+    <div class="col-md-3 sidenav hidden-xs">
+        <a href="<?=BASE_LINK_URL?>"><img src="inc/images/logo.gif" width="407" height="65" alt="Twando" /></a>
+        <!--<h2>Logo</h2>-->
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Dashboard</a></li>
+        <li><a href="#section2">Accounts</a></li>
+        <li><a href="/multi_account_functions.php">Multi-Account Functions</a></li>
+        <li><a href="/cron_instructions.php">Cron Job Instructions</a></li>
+      </ul>
 
-
+    </div>
