d81f87433cdfe9d2c26af8949bf30e5f5fa82795
D4mer <d4mer@d4mations.com>
2017-06-02 18:14:51 +0200

    more styling of the forms

--- inc/content/english/ajax.multi_account_functions_inc.php
+++ inc/content/english/ajax.multi_account_functions_inc.php
@@ -40,23 +40,26 @@ http://www.twando.com/
 <?=$response_msg?>
 <form method="post"  class="form-horizontal" action="" name="allfollow_form" id="allfollow_form"
       onsubmit="ajax_multi_account_update('tab2','allfollow_form'); return false;">
-    <div class="col-md-12">
-        <br />&nbsp;&nbsp;<select name="cross_op" id="cross_op" class="btn btn-primary dropdown-toggle" style="width: 95px;">
-         <option value="1" selected="selected">Follow</option>
-         <option value="2">Unfollow</option>
-        </select>&nbsp;&nbsp;all the following&nbsp;&nbsp;<select name="cross_type" id="cross_type" class="btn btn-primary dropdown-toggle" style="width: 120px;">
-         <option value="1" selected="selected">screen names</option>
-         <option value="2">Twitter ids</option>
-        </select>&nbsp;&nbsp;from all your Twitter accounts (enter one screen name / Twitter id per line):<br />
-    </div>
-</form>
-        <textarea name="twitter_ids_list" id="twitter_ids_list" class="form-control" style="height: 249px; margin: 6px 0px 0px 0px;"></textarea><br />
+    <div class="form-group">
+        <div class="col-md-12">
+            <br />&nbsp;&nbsp;<select name="cross_op" id="cross_op" class="btn btn-primary dropdown-toggle" style="width: 95px;">
+             <option value="1" selected="selected">Follow</option>
+             <option value="2">Unfollow</option>
+            </select>&nbsp;&nbsp;all the following&nbsp;&nbsp;<select name="cross_type" id="cross_type" class="btn btn-primary dropdown-toggle" style="width: 120px;">
+             <option value="1" selected="selected">screen names</option>
+             <option value="2">Twitter ids</option>
+            </select>&nbsp;&nbsp;from all your Twitter accounts (enter one screen name / Twitter id per line):<br />
+        </div>
+
+        <textarea name="twitter_ids_list" id="twitter_ids_list" class="form-control" style="height: 200px; margin: 6px;
+                                                                                            width: 200px;"></textarea><br />
 
         <input type="hidden" name="a" value="allfollow_go" />
 
         <input type="submit" value="All Follow / Unfollow" class="btn btn-primary"
                onclick="ajax_multi_account_update('tab2','allfollow_form');" />
-
+    </div>
+</form>
 <?php
    break;
    case 'tab3':
