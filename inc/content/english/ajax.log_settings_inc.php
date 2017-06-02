f9ee04ca8d5bd493e7a44e3334e6192a5b401b96
D4mer <d4mer@d4mations.com>
2017-06-02 16:13:47 +0200

    More formatting and styling

--- inc/content/english/ajax.log_settings_inc.php
+++ inc/content/english/ajax.log_settings_inc.php
@@ -56,14 +56,14 @@ http://www.twando.com/
 
 </div>
 <h2>View <?=$tab_vars['h2']?> Logs</h2>
-<table class="data_table">
- <tr>
-  <td class="heading">Log Content</td>
+<table id="results_table" class="datatable table table-striped table-responsive table-condensed">
+ <thead>
+  <th>Log Content</th>
   <?php if ($tab_vars['type'] == 1) { ?>
-  <td class="heading" width="445">Affected Users</td>
+  <th>Affected Users</th>
   <?php } ?>
-  <td class="heading" width="130">Log Time</td>
- </tr>
+  <th>Log Time</th>
+ </thead>
 <?php
 $q2_base = "SELECT SQL_CALC_FOUND_ROWS id, log_text, affected_users, last_updated FROM " . DB_PREFIX . "cron_logs WHERE owner_id='" . $db->prep($q1a['id']) . "' AND type='" . (int)$tab_vars['type'] . "' ORDER BY last_updated DESC ";
 
@@ -77,50 +77,55 @@ if ($total_items > 0) {
 $row_count = $db->num_rows($q2);
 while ($q2a = $db->fetch_array($q2)) {
 ?>
+<tbody>
  <tr>
-  <td><?=htmlspecialchars($q2a['log_text'])?></td>
+    <td>
+      <?=htmlspecialchars($q2a['log_text'])?>
+    </td>
   <?php if ($tab_vars['type'] == 1) { ?>
-  <td>
-<?php
- $aff_users = unserialize($q2a['affected_users']);
- $count = 0;
- if (is_array($aff_users)) {
-  foreach ($aff_users as $this_id) {
-   if ($count <= 169) {
-    //Limit to showing first 169 due to potential page size
-    $db->print_au($this_id,array("Screen Name","Name","Following","Followers","Data Cached"));
-   }
-   $count ++;
-  }
- }
-?>
-  </td>
+    <td>
+        <?php
+         $aff_users = unserialize($q2a['affected_users']);
+         $count = 0;
+         if (is_array($aff_users)) {
+          foreach ($aff_users as $this_id) {
+           if ($count <= 169) {
+            //Limit to showing first 169 due to potential page size
+            $db->print_au($this_id,array("Screen Name","Name","Following","Followers","Data Cached"));
+           }
+           $count ++;
+          }
+         }
+        ?>
+    </td>
   <?php } ?>
-  <td><?=date(TIMESTAMP_FORMAT,strtotime($q2a['last_updated']))?></td>
+    <td>
+      <?=date(TIMESTAMP_FORMAT,strtotime($q2a['last_updated']))?>
+    </td>
  </tr>
+</tbody>
 <?php
 }
 ?>
-</table>
-<div class="page_nav">
+<div class="pagination">
  <div class="left_side">
  <?=$total_items?> result<?php if ($total_items != 1) {echo 's';}?> found; showing page <?=$page?> of <?=$total_pages?>.
  </div>
- <div class="right_side">
+ <ul class="pagination">
 <?php
 if ($total_pages > 1) {
 ?>
-  <div class="box"><?=$back_string?></div>
+  <li><?=$back_string?></li>
   <?=$page_list_string?>
-  <div class="box"><?=$next_string?></div>
+  <li><?=$next_string?></li>
 <?php
 } else {
 ?>
- <div class="boxw">&nbsp;</div>
+ <div>&nbsp;</div>
 <?php
 }
 ?>
- </div>
+ </ul>
 </div>
 <?php
 //If total items > 0
@@ -139,23 +144,27 @@ if ($total_pages > 1) {
    break;
    case 'tab4':
 ?>
-<h2>Purge Log History</h2>
-<?=$response_msg?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h2>Purge Log History</h2>
+        <?=$response_msg?>
+    </div>
 <form method="post" action="" name="log_purge_form" id="log_purge_form" onsubmit="ajax_log_settings_update('tab4','log_purge_form'); return false;">
-Delete&nbsp;&nbsp;<select name="log_type" id="log_type" class="input_box_style" style="width: 90px;">
+Delete&nbsp;&nbsp;<select name="log_type" id="log_type" class="btn btn-primary dropdown-toggle">
  <option value="0" selected="selected">all logs</option>
  <option value="1">follow logs</option>
  <option value="2">tweet logs</option>
-</select>&nbsp;&nbsp;for this account&nbsp;&nbsp;<select name="log_time" id="log_time" class="input_box_style" style="width: 150px;">
+</select>&nbsp;&nbsp;for this account&nbsp;&nbsp;<select name="log_time" id="log_time" class="btn btn-primary dropdown-toggle">
  <option value="1" selected="selected">from the beginning</option>
  <option value="2">older than 30 days</option>
  <option value="3">older than 90 days</option>
 </select>
 <br />
-<input type="checkbox" name="empty_cache" id="empty_cache" value="1" /> Empty user cache?<br />
+<input type="checkbox" name="empty_cache" id="empty_cache" value="1" class="checkbox"/> Empty user cache?<br />
 <input type="hidden" name="a" value="deletelogs" />
 </form>
-<input type="submit" value="Purge Logs" class="submit_button_style" onclick="ajax_log_settings_update('tab4','log_purge_form');" />
+<input type="submit" value="Purge Logs" class="btn btn-primary" onclick="ajax_log_settings_update('tab4','log_purge_form');" />
+</div>
 <?php
    break;
   //End of switch
