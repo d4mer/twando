6aacddd09123872d2962830392d239211a3ba57d
D4mer <d4mer@d4mations.com>
2017-06-02 17:33:25 +0200

    formatting and styling

--- inc/content/english/follow_settings.php
+++ inc/content/english/follow_settings.php
@@ -15,19 +15,23 @@ if ($q1a['id'] == "")  {
  echo mainFuncs::push_response(7);
 } else {
 ?>
-<div class="tab_row">
-<input type="hidden" name="twitter_id" id="twitter_id" value="<?=$q1a['id']?>" />
-<div class="tab_main" id="tab1">
- <a href="javascript:ajax_follow_settings_tab('tab1');">Auto Follow Settings</a>
-</div><div class="tab_main" id="tab2">
- <a href="javascript:ajax_follow_settings_tab('tab2');">Unfollow Exclusions</a>
-</div><div class="tab_main" id="tab3">
- <a href="javascript:ajax_follow_settings_tab('tab3');">Follow Exclusions</a>
-</div><div class="tab_main" id="tab4">
- <a href="javascript:ajax_follow_settings_tab('tab4');">Auto DM Message</a>
-</div><div class="tab_main" id="tab5">
- <a href="javascript:ajax_follow_settings_tab('tab5');">Search to Follow</a>
-</div>
+<div class="nav nav-tabs">
+    <input type="hidden" name="twitter_id" id="twitter_id" value="<?=$q1a['id']?>" />
+    <li class="tab_main" id="tab1">
+        <a href="javascript:ajax_follow_settings_tab('tab1');">Auto Follow Settings</a>
+    </li>
+    <li class="tab_main" id="tab2">
+        <a href="javascript:ajax_follow_settings_tab('tab2');">Unfollow Exclusions</a>
+    </li>
+    <li class="tab_main" id="tab3">
+        <a href="javascript:ajax_follow_settings_tab('tab3');">Follow Exclusions</a>
+    </li>
+    <li class="tab_main" id="tab4">
+        <a href="javascript:ajax_follow_settings_tab('tab4');">Auto DM Message</a>
+    </li>
+    <li class="tab_main" id="tab5">
+        <a href="javascript:ajax_follow_settings_tab('tab5');">Search to Follow</a>
+    </li>
 </div>
 
 <br style="clear: both;" />
